36fab3448f6dffff133bdfa1879068973aade744
erfanms <60511091+erfanms@users.noreply.github.com>
2022-08-18 16:52:29 +0530

    Update stream_deploy.py

--- stream_deploy.py
+++ stream_deploy.py
@@ -1,11 +1,5 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
-
-
-
-
-uploaded_parameter_file = st.file_uploader("Upload parameter file")
-if uploaded_parameter_file is not None:
-    df_parameters = pd.read_excel(uploaded_parameter_file)
-
+import pandas as pd
+st.title(“A Simple Streamlit Web App”)
+name = st.text_input(“Enter your name”, ‘’)
+st.write(f”Hello {name}!”)
