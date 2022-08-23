0b903f20ae8af0f4fc7ecf758071a2dbfa9e5129
erfanms <60511091+erfanms@users.noreply.github.com>
2022-08-23 14:34:21 +0530

    Update stream_deploy.py

--- stream_deploy.py
+++ stream_deploy.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
-st.title(“A Simple Streamlit Web App”)
-name = st.text_input(“Enter your name”, ‘’)
-st.write(f”Hello {name}!”)
+st.title("A Simple Streamlit Web App")
+name = st.text_input("Enter your name", '')
+st.write(f"Hello {name}!")
